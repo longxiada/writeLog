99a49a79bc748b12a17fec341b7ac9927de5a509
longxiada <15166657913@163.com>
2019-04-03 16:10:09 +0800

    123

---
+++ Log.cs
@@ -0,0 +1,63 @@
+﻿#region
+using System;
+#endregion
+
+namespace LogWriter
+{
+
+    /// <summary>
+    /// Log container object
+    /// </summary>
+    public class Log
+    {
+
+        /// <summary>
+        /// Content of log
+        /// </summary>
+        private string LogMessage;
+
+        /// <summary>
+        /// Log timestamp
+        /// </summary>
+        private DateTime LogTime;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="message">Logged message</param>
+        public Log(string message)
+        {
+            LogMessage = message;
+            LogTime = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Log message accessor
+        /// </summary>
+        /// <returns>Log message</returns>
+        public string GetMessage()
+        {
+            return LogMessage;
+        }
+
+        /// <summary>
+        /// Get the time from log timestamp
+        /// </summary>
+        /// <returns>Time</returns>
+        public string GetTime()
+        {
+            return LogTime.ToString("hh:mm:ss.fff tt");
+        }
+
+        /// <summary>
+        /// Get the date from log timestamp
+        /// </summary>
+        /// <returns>Date</returns>
+        public string GetDate()
+        {
+            return LogTime.ToString("yyyy-MM-dd");
+        }
+
+    }
+
+}
